3afb0ffb03bb62fa42b089f3859e87911043dd4d
CristianJaldinPereira <jaldinpereiracristian@gmail.com>
2025-08-01 16:52:02 -0400

    prueba unitaria

---
+++ EjerciciosEnClase/EjemploJUnitAnt/src/ejemplojunitant/Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplojunitant;
+
+/**
+ *
+ * @author CJ
+ */
+public class Calculadora {
+public int sumar(int a, int b) {
+return a + b;
+}
+public int restar(int a, int b) {
+return a - b;
+}
+public int dividir(int a, int b) {
+if (b == 0) throw new IllegalArgumentException("No se puede dividir por cero");
+return a / b;
+}
+}
